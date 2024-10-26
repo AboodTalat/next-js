a7706d2197d795e9cd44c6d30aaba674eb7b3459
AboodTalat <aboodtalat@gmail.com>
2024-10-26 22:48:26 +0300

    Fixed stuff

--- public/sw.js
+++ public/sw.js
@@ -5,39 +5,7 @@ workbox.routing.registerRoute(({ request }) => request.destination === "image", 
 const CACHE_NAME = "pwa-cache-v1";
 const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/images/icon.png"];
 
-// // Install event
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
 
-// Fetch event to serve cached resources
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
-
-// Activate event to update the cache
-// self.addEventListener("activate", (event) => {
-//   const cacheWhitelist = [CACHE_NAME];
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           if (!cacheWhitelist.includes(cacheName)) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
 
 self.addEventListener("push", (event) => {
   let data = {};
