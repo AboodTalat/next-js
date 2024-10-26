461eeb1b0a5d6bbf7091f1d2460a87d76fca7e0b
AboodTalat <aboodtalat@gmail.com>
2024-10-26 22:45:24 +0300

    Fixed stuff

--- public/sw.js
+++ public/sw.js
@@ -15,29 +15,29 @@ const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/images/i
 // });
 
 // Fetch event to serve cached resources
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
 
 // Activate event to update the cache
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (!cacheWhitelist.includes(cacheName)) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
 self.addEventListener("push", (event) => {
   let data = {};
